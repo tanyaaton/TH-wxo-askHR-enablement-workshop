e46f379127b657c4a6a960aecdb863acba68e73a
tanyaaton <tanyatonoran@gmail.com>
2025-12-01 14:20:32 +0700

    feat: delete LAB_2_AIGATEWAY

--- LAB_2_AI_GATEWAY/groq/add_model.sh
+++
@@ -1,3 +0,0 @@
-# orchestrate models add --name watsonx/meta-llama/llama-4-maverick-17b-128e-instruct-fp8 --app-id watsonx_credentials
-
-orchestrate models import --file groq-gpt-model.yaml --app-id groq_creds
